046695cef287afb0050c49a6e84b6b98623f5195
Marco Tulio <marcotulio@inco.vc>
2025-08-12 17:21:56 -0300

    Ajusta sintaxe da saída da DailyRetuns.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class CustomInstallScripts(InstallScripts):
 
 setup(
     name='fincore',
-    version='4.7.0',
+    version='4.7.1',
     description='A financial core library',
     author='Rafael Viotti',
     author_email='viotti@inco.vc',
